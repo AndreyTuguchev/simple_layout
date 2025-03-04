c44b841c9dca49574850a220b7d89a22dbbe20f9
Andrey Tuguchev <tuguchev.andrey@gmail.com>
2025-03-04 20:16:16 +0500

    feat: footer implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import earth_image from './assets/images/earth.png';
 import hero_bg_md from './assets/images/hero-bg-md.jpg';
+import rocket from './assets/images/rocket.svg';
 import space_bg_1 from './assets/images/space-bg-1.jpg';
 import space_bg_2 from './assets/images/space-bg-2.jpg';
 import space_bg_3 from './assets/images/space-bg-3.jpg';
@@ -13,8 +14,7 @@ function App() {
   return (
     <>
     <div className="App">
-    
-    
+   
     <section className='section hero section__hover' style={{backgroundImage: `url(${hero_bg_md})`}}>
       <div className='section-inner '>
         <div className='hero-image'>
@@ -119,8 +119,25 @@ function App() {
       </section>
 
     </main>
+    <section className='section bottom'>
+      <div className='section-inner'>
+        <div className='bottom__title'>Embark on a space journey</div>
+        <div className='bottom__text'>Travelling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed of exploring stars, planets and galaxies, then our company is ready to help you realize this dream. We offer a unique experience that will allow you to go on a space journey and see all the secrets of the universe. We guarantee that every moment in space will be filled with incredible impressions, excitement and new discoveries. Our team of professionals takes care of your safety and comfort so that you can fully enjoy your adventure in space. We offer various options for space excursions.</div>
+        <div className='bottom__link'>
+          <a href="/#readmore" alt=" ">Read More</a>
+        </div>
+      </div>
+    </section>
     
     </div>
+    <footer className='footer'>
+      <div className='footer__inner'>
+        <div className='footer__image'>
+          <img src={rocket} alt="" />
+        </div>
+        <div className='footer__title'>Exciting space adventure!</div>
+      </div>
+    </footer>
 
     </>
   );
